8ea475257a8ea064677e9ebf4010aad2b36421a3
Lisha Patil <lisha.patil@flexc.work>
2023-10-09 01:45:26 +0200

    commit

--- DockerDotNet/Program.cs
+++ DockerDotNet/Program.cs
@@ -1,8 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+//comments
+app.MapGet("/time", () => Results.Ok(DateTime.UtcNow));
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+await app.RunAsync();
